38500bd19f0f1112c97a33d05b411588d0b6d867
“Calvin” <calvin34585@gmail.com>
2018-10-11 23:17:38 -0700

    added animated prop

--- index.js
+++ index.js
@@ -1,32 +1,57 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { View, Platform } from 'react-native';
+import { Animated, Platform, View } from "react-native";
+import React, { Component } from "react";
 
-export default class ElevatedView extends React.Component {
+import PropTypes from "prop-types";
+
+export default class ElevatedView extends Component {
   static propTypes = {
     elevation: PropTypes.number,
+    animated: PropTypes.bool
   };
   static defaultProps = {
-    elevation: 0
+    elevation: 0,
+    animated: false
   };
 
   render() {
     const { elevation, style, ...otherProps } = this.props;
 
-    if (Platform.OS === 'android') {
-      return (
-        <View elevation={elevation} style={[{ elevation, backgroundColor: 'white' }, style ]} {...otherProps}>
-          {this.props.children}
-        </View>
-      );
+    if (Platform.OS === "android") {
+      if (this.props.animated)
+        return (
+          <Animated.View
+            elevation={elevation}
+            style={[{ elevation, backgroundColor: "white" }, style]}
+            {...otherProps}
+          >
+            {this.props.children}
+          </Animated.View>
+        );
+      else
+        return (
+          <View
+            elevation={elevation}
+            style={[{ elevation, backgroundColor: "white" }, style]}
+            {...otherProps}
+          >
+            {this.props.children}
+          </View>
+        );
     }
 
     if (elevation === 0) {
-      return (
-        <View style={style} {...otherProps}>
-          {this.props.children}
-        </View>
-      )
+      if (this.props.animated)
+        return (
+          <Animated.View style={style} {...otherProps}>
+            {this.props.children}
+          </Animated.View>
+        );
+      else
+        return (
+          <View style={style} {...otherProps}>
+            {this.props.children}
+          </View>
+        );
     }
 
     //calculate iosShadows here
@@ -34,14 +59,21 @@ export default class ElevatedView extends React.Component {
       shadowOpacity: 0.0015 * elevation + 0.18,
       shadowRadius: 0.54 * elevation,
       shadowOffset: {
-        height: 0.6 * elevation,
-      },
+        height: 0.6 * elevation
+      }
     };
 
-    return (
-      <View style={[iosShadowElevation, style]} {...otherProps}>
-        {this.props.children}
-      </View>
-    );
+    if (this.props.animated)
+      return (
+        <Animated.View style={[iosShadowElevation, style]} {...otherProps}>
+          {this.props.children}
+        </Animated.View>
+      );
+    else
+      return (
+        <View style={[iosShadowElevation, style]} {...otherProps}>
+          {this.props.children}
+        </View>
+      );
   }
 }
